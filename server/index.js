b74f339d7fabfa03d9ce19d91c451b0595deba18
Thomas Kelly <tmkelly28@gmail.com>
2017-04-24 15:33:56 -0400

    update server and add favicon

--- server/index.js
+++ server/index.js
@@ -21,30 +21,44 @@ passport.deserializeUser((id, done) =>
     .then(user => done(null, user))
     .catch(done));
 
-store.sync()
-  .then(() => db.sync()
-  .then(() => app
-    .use(morgan('dev'))
-    .use(express.static(path.join(__dirname, '..', 'public')))
-    .use(bodyParser.json())
-    .use(bodyParser.urlencoded({ extended: true }))
-    .use(session({
-      secret: process.env.SESSION_SECRET || 'my best friend is Cody',
-      store,
-      resave: false,
-      saveUninitialized: false
-    }))
-    .use(passport.initialize())
-    .use(passport.session())
-    .use('/auth', require('./auth'))
-    .use('/api', require('./api'))
-    .use((req, res, next) =>
-      path.extname(req.path).length > 0 ? res.status(404).send('Not found') : next())
-    .use('*', (req, res, next) =>
-      res.sendFile(path.join(__dirname, '..', 'public/index.html')))
-    .use((err, req, res, next) =>
-      res.status(err.status || 500).send(err.message || 'Internal server error.'))
-    .listen(PORT, () =>
-      console.log(`Mixing it up on port ${PORT}`))
-  )
-);
+const createApp = () => app
+  .use(morgan('dev'))
+  .use(express.static(path.join(__dirname, '..', 'public')))
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(session({
+    secret: process.env.SESSION_SECRET || 'my best friend is Cody',
+    store,
+    resave: false,
+    saveUninitialized: false
+  }))
+  .use(passport.initialize())
+  .use(passport.session())
+  .use('/auth', require('./auth'))
+  .use('/api', require('./api'))
+  .use((req, res, next) =>
+    path.extname(req.path).length > 0 ? res.status(404).send('Not found') : next())
+  .use('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '..', 'public/index.html')))
+  .use((err, req, res, next) =>
+    res.status(err.status || 500).send(err.message || 'Internal server error.'));
+
+const syncDb = () =>
+  db.sync();
+
+const listenUp = () =>
+  app.listen(PORT, () =>
+    console.log(`Mixing it up on port ${PORT}`));
+
+// This evaluates as true when this file is run directly from the command line,
+// i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
+// It will evaluate false when this module is required by another module - for example,
+// if we wanted to require our app in a test spec
+if (require.main === module) {
+  store.sync()
+    .then(syncDb)
+    .then(createApp)
+    .then(listenUp);
+} else {
+  createApp(app);
+}
