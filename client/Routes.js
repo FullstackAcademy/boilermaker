25aecaef5c27530c1f4280d642ffbc8366d35ea8
Thomas Kelly <tmkelly28@gmail.com>
2017-07-18 12:02:39 -0400

    add some more comments

--- client/Routes.js
+++ client/Routes.js
@@ -48,6 +48,8 @@ class Routes extends Component {
  */
 const mapState = (state) => {
   return {
+    // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
+    // Otherwise, state.user will be an empty object, and state.user.id will be falsey
     isLoggedIn: !!state.user.id
   }
 }
--- client/components/index.js
+++ client/components/index.js
@@ -1,3 +1,8 @@
+/**
+ * `components/index.js` exists simply as a 'central export' for our components.
+ * This way, we can import all of our components from the same place, rather than
+ * having to figure out which file they belong to!
+ */
 export {default as Main} from './Main'
 export {default as UserHome} from './UserHome'
 export {Login, Signup} from './AuthForm'
