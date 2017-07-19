b01491f3c75bff6cdef4ce207d2893898697eeb3
Thomas Kelly <tmkelly28@gmail.com>
2017-07-19 15:01:15 -0400

    add sockets

--- client/index.js
+++ client/index.js
@@ -5,6 +5,9 @@ import {Provider} from 'react-redux'
 import store from './store'
 import Routes from './routes'
 
+// establishes socket connection
+import socket from './socket'
+
 ReactDOM.render(
   <Provider store={store}>
     <Routes />
---
+++ server/socket/index.js
@@ -0,0 +1,10 @@
+module.exports = (io) => {
+
+  io.on('connection', (socket) => {
+    console.log(`A socket connection to the server has been made: ${socket.id}`)
+
+    socket.on('disconnect', () => {
+      console.log(`Connection ${socket.id} has left the building`)
+    })
+  })
+}
