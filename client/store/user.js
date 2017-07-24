7e57369760028698fd1057f2b40b78f331ca0277
Thomas Kelly <tmkelly28@gmail.com>
2017-07-24 17:20:37 -0400

    forgot to catch

--- client/store/user.js
+++ client/store/user.js
@@ -26,6 +26,7 @@ export const me = () =>
     axios.get('/auth/me')
       .then(res =>
         dispatch(getUser(res.data || defaultUser)))
+      .catch(err => console.log(err))
 
 export const auth = (email, password, method) =>
   dispatch =>
