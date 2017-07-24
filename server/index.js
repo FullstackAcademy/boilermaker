b75be5e30134c6e662d3280fa9a0f7136d47e7e0
Thomas Kelly <tmkelly28@gmail.com>
2017-07-24 09:42:43 -0400

    log the error stack

--- server/index.js
+++ server/index.js
@@ -73,6 +73,8 @@ const createApp = () => {
 
   // error handling endware
   app.use((err, req, res, next) => {
+    console.error(err)
+    console.error(err.stack)
     res.status(err.status || 500).send(err.message || 'Internal server error.')
   })
 
