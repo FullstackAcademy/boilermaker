6e606348fd7fe9f8b2342e5c0700e996bf9e6f83
Thomas Kelly <tmkelly28@gmail.com>
2017-08-14 12:34:59 -0400

    update script to force push

--- deploy.sh
+++ deploy.sh
@@ -11,30 +11,24 @@ echo 'Prepare to deploy...'
 # or this step will fail!
 git checkout -b deploy
 
-# 2. Pull our heroku remote master branch into our local 'deploy' branch,
-# so that your local 'deploy' branch is 'up-to-date' with the heroku master branch.
-# You may see an error at this step when you do your initial deploy, because the heroku
-# master branch isn't set up yet, but that's okay.
-git pull heroku master
-
-# 3. Run webpack with -p for 'production' mode. This builds our bundle.js and bundle.js.map
+# 2. Run webpack with -p for 'production' mode. This builds our bundle.js and bundle.js.map
 webpack -p
 
-# 4. Add our normally ignored bundle.js and bundle.js.map files with -f for force.
+# 3. Add our normally ignored bundle.js and bundle.js.map files with -f for force.
 # This ensures that our build files are included on our production server.
 git add -f public/bundle.js public/bundle.js.map
 
-# 5. Make a commit (which could be empty, in case we want to force a re-deploy)
+# 4. Make a commit (which could be empty, in case we want to force a re-deploy)
 git commit --allow-empty -m 'Deploying'
 
-# 6. Push our local 'deploy' branch to the heroku remote 'master' branch
+# 5. Push our local 'deploy' branch to the heroku remote 'master' branch (with the --force flag, because we want to overwrite whatever's already up there without worrying about merging it in locally first)
 # This triggers the process whereby heroku rebuilds our app on our dyno
-git push heroku deploy:master
+git push --force heroku deploy:master
 
-# 7. Switch back to our local master branch
+# 6. Switch back to our local master branch
 git checkout master
 
-# 8. Delete our dummy 'deploy' branch
+# 7. Delete our dummy 'deploy' branch
 git branch -D deploy
 
 echo 'Deployment complete!'
