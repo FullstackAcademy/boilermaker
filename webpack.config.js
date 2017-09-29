278b3d1379430d6550eef043e6e300cd7c08c78e
Thomas Kelly <tmkelly28@gmail.com>
2017-09-29 15:49:09 -0400

    typo

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
       }
     ]
   },
-  // When we're development, we can use this handy live-reload plugin
+  // When we're in development, we can use this handy live-reload plugin
   // to refresh the page for us every time we make a change to our client-side
   // files. It's like `nodemon` for the front end!
   plugins: isDev ? [new LiveReloadPlugin({appendScriptTag: true})] : []
