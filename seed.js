aad4e726a14b0f45e207586ffd1276d1854f0d06
Thomas Kelly <tmkelly28@gmail.com>
2017-09-29 11:32:23 -0400

    changes per review

--- seed.js
+++ seed.js
@@ -14,6 +14,7 @@ const {User} = require('./server/db/models')
 
 async function seed () {
   await db.sync({force: true})
+  console.log('db synced!')
   // Whoa! Because we `await` the promise that db.sync returns, the next line will not be
   // executed until that promise resolves!
 
@@ -23,23 +24,28 @@ async function seed () {
   ])
   // Wowzers! We can even `await` on the right-hand side of the assignment operator
   // and store the result that the promise resolves to in a variable! This is nice!
-  console.log(`Seeded ${users.length} users`)
-  console.log(`Seeded successfully`)
-  process.exit(0)
+  console.log(`seeded ${users.length} users`)
+  console.log(`seeded successfully`)
 }
 
 // Execute the `seed` function
 // `Async` functions always return a promise, so we can use `catch` to handle any errors
 // that might occur inside of `seed`
-seed().catch(err => {
-  console.error(err.message)
-  console.error(err.stack)
-  process.exit(1)
-})
+seed()
+  .catch(err => {
+    console.error(err.message)
+    console.error(err.stack)
+    process.exitCode = 1
+  })
+  .then(() => {
+    console.log('closing db connection')
+    db.close()
+    console.log('db connection closed')
+  })
 
 /*
  * note: everything outside of the async function is totally synchronous
  * The console.log below will occur before any of the logs that occur inside
  * of the async function
  */
-console.log('Seeding...')
+console.log('seeding...')
