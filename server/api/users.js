fbf055c81c94563b65633f1cb08db05c59eb5b21
Thomas Kelly <tmkelly28@gmail.com>
2017-10-04 14:58:31 -0400

    remove isAdmin from regular get, arbitrary

--- server/api/users.js
+++ server/api/users.js
@@ -23,7 +23,7 @@ router.get('/', (req, res, next) => {
     // explicitly select only the id and email fields - even though
     // users' passwords are encrypted, it won't help if we just
     // send everything to anyone who asks!
-    attributes: ['id', 'email', 'isAdmin']
+    attributes: ['id', 'email']
   })
     .then(users => res.json(users))
     .catch(next)
