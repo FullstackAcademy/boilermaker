e54a621a4a79a7720a3d5e109245e819e25b23f9
Jeanne Castillo <jcaron27@gmail.com>
2017-10-31 17:27:16 -0400

    Configure project

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,10 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', 'es2015']
+        }
       },
       {
         test: /\.scss$/,
