1bd64d4b06925ad7569417fc98eb7ed9bdf72bad
Thomas Kelly <tmkelly28@gmail.com>
2018-01-07 14:19:23 -0500

    add app to front of .use

--- server/index.js
+++ server/index.js
@@ -59,7 +59,7 @@ const createApp = () => {
   app.use(express.static(path.join(__dirname, '..', 'public')))
 
   // any remaining requests with an extension (.js, .css, etc.) send 404
-  .use((req, res, next) => {
+  app.use((req, res, next) => {
     if (path.extname(req.path).length) {
       const err = new Error('Not found')
       err.status = 404
