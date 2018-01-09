b2f1d5036e382a386d24e57c00bd28581c1ef731
bryanhclark <bryanhclark@gmail.com>
2018-01-09 15:30:37 -0500

    first commit

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 const db = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost:5432/boilermaker', {
+  process.env.DATABASE_URL || 'postgres://localhost:5432/graceShopper', {
     logging: false
   }
 )
