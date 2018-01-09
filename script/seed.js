f61b35d38abc41ce8f069f3167ddd5bd3a5dd035
Trey <treycoopermusic@gmail.com>
2018-01-08 21:28:33 -0500

    add dummy reviews

--- script/seed.js
+++ script/seed.js
@@ -10,7 +10,7 @@
  * Now that you've got the main idea, check it out in practice below!
  */
 const db = require('../server/db');
-const {User, Category, Product} = require('../server/db/models');
+const {User, Category, Product, Review } = require('../server/db/models');
 
 async function seed () {
   await db.sync({force: true})
@@ -41,7 +41,7 @@ async function seed () {
   const products = await Promise.all([
     Product.create({name: "Wheat Noodles",
     price: 8.00, description: "Tasty and made with classic wheat", inventoryCount: 50,
-    size: 50, categoryId: 4}),Product.create({name: "Egg Noodles", 
+    size: 50, categoryId: 4}),Product.create({name: "Egg Noodles",
     image: "https://thefoodistuk.files.wordpress.com/2015/01/p1060025.jpg",
     price: 10.00, description: "Tasty and made with eggs", inventoryCount: 100,
     size: 60, categoryId: 2}),
@@ -54,9 +54,14 @@ async function seed () {
     price: 12.50, description: "Tasty and made with sweet potatoes, zucchini and butternut squash", inventoryCount: 200,
     size: 65, categoryId: 1})
   ])
+
+  const reviews = await Promise.all([
+    Review.create({body: "These noodles are the best noodles that I've ever had in my life! I am going to recommend to everyone I meet!", rating: 5, userId: 1, productId: 2}),
+    Review.create({body: "Ramenzon's egg noodles are great! Since I am not vegan, I eat them everyday!", rating: 5, userId: 2, productId: 1}),
+    Review.create({body: "These noodles are incredible! Why eat rice when you can eat Ramenzon's rice noodles!?!", rating: 5, userId: 2, productId: 3}),
+    Review.create({body: "Ramenzon's veggie noodles are delicious and healthy. Thanks to Ramenzon, I can trick my kids into eating vegatables!", rating: 5, userId: 1, productId: 4})
+  ])
 }
-
-
 
 
 // Execute the `seed` function
