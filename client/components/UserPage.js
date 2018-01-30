ff2980cd6837516f44fdb0e3f76815dbf67546f5
Ari Kramer <arikramer24@gmail.com>
2018-01-30 14:43:51 -0500

    added progress bar to userpage

--- client/components/UserPage.js
+++ client/components/UserPage.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { fetchUser } from '../store';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { Col, Image } from 'react-bootstrap';
+import { Image } from 'react-bootstrap';
+import Score from './UserPage-Score';
 
 class UserPage extends Component {
     componentDidMount() {
@@ -17,7 +18,11 @@ class UserPage extends Component {
                 <Image src={user.photoURL} rounded className="user-page-user-image" />
                 <div className="user-page-header-text">
                     <h1>{user.name}</h1>
-                    <h3>Score: {user.score}</h3>
+                    <h3>Cred</h3>
+                    <h4>Level: {Math.floor(user.score / 100)}</h4>
+                    <h5>Level progress</h5>
+                    <Score user={user} />
+                    {/*current room*/}
                 </div>
             </div>
         )
