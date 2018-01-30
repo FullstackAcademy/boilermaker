dbf8681a26861357d389e5970761cb8c33d42492
i-am-jabriel <officialjabe@gmail.com>
2018-01-30 11:10:41 -0500

    reverted /public changes

--- server/index.js
+++ server/index.js
@@ -58,7 +58,7 @@ const createApp = () => {
   app.use('/api', require('./api'))
 
   // static file-serving middleware
-  app.use(express.static(path.join(__dirname, '..', '/public')))
+  app.use(express.static(path.join(__dirname, '..', 'public')))
 
   // any remaining requests with an extension (.js, .css, etc.) send 404
   app.use((req, res, next) => {
@@ -73,7 +73,7 @@ const createApp = () => {
 
   // sends index.html
   app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', '/public/index.html'))
+    res.sendFile(path.join(__dirname, '..', 'public/index.html'))
   })
 
   // error handling endware
