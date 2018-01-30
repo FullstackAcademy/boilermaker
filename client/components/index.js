9bf4871e00998956bab59e369f513d1454656cbb
i-am-jabriel <officialjabe@gmail.com>
2018-01-30 10:31:25 -0500

    changed Main.js to main.js

--- client/components/index.js
+++ client/components/index.js
@@ -3,7 +3,7 @@
  * This way, we can import all of our components from the same place, rather than
  * having to figure out which file they belong to!
  */
-export { default as Main } from './Main'
+export { default as Main } from './main'
 export { default as Channel } from './Channel'
 export { default as UserHome } from './user-home'
 export { default as Home } from './Home'
