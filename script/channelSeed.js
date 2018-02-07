aa09afb57cfcc78eee937659dc34e04ab2bb620e
Shayan Sheikh <najam.shayan@gmail.com>
2018-02-07 12:24:41 -0500

    Cleaned up seed files

--- script/channelSeed.js
+++ script/channelSeed.js
@@ -1,25 +1,24 @@
-const db = require('../server/db')
 const { Channel } = require('../server/db/models')
 
 const seedChannels = async function () {
   const channels = await Promise.all([
-    Channel.create({ name: 'Politico Hardos', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 2, categoryId: 1, size: 250 }),
+    Channel.create({ name: 'Politico', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 2, categoryId: 1, size: 250 }),
     Channel.create({ name: 'NYC Town Hall', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 3, categoryId: 1, size: 200 }),
 
-    Channel.create({ name: 'Herro Prease', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 1, categoryId: 3, size: 150 }),
+    Channel.create({ name: 'Anime was a mistake', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 1, categoryId: 2, size: 150 }),
 
-    Channel.create({ name: 'Welcome to the Jungle', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 4, categoryId: 2, size: 100 }),
-    Channel.create({ name: 'EA Lovers', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 5, categoryId: 2, size: 50 }),
-    Channel.create({ name: 'Fanboys Xbox vs PS4', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 6, categoryId: 2, size: 200 }),
-    Channel.create({ name: 'Get Smashed', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 6, categoryId: 2, size: 125 }),
+    Channel.create({ name: 'Welcome to the Jungle', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 4, categoryId: 3, size: 100 }),
+    Channel.create({ name: 'EA Lovers', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 5, categoryId: 3, size: 50 }),
+    Channel.create({ name: 'Fanboys Xbox vs PS4', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 6, categoryId: 3, size: 200 }),
+    Channel.create({ name: 'Get Smashed', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 6, categoryId: 3, size: 125 }),
 
-    Channel.create({ name: 'Winter is Coming', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 5, categoryId: 5, size: 200 }),
-    Channel.create({ name: 'Oscars Predictions', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 4, categoryId: 5, size: 250 }),
-    Channel.create({ name: 'Basic Girls', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 4, categoryId: 5, size: 75 }),
+    Channel.create({ name: 'Winter is Coming', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 5, categoryId: 4, size: 200 }),
+    Channel.create({ name: 'Oscars Predictions', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 4, categoryId: 4, size: 250 }),
+    Channel.create({ name: 'Basic Girls', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 4, categoryId: 4, size: 75 }),
 
-    Channel.create({ name: 'Roger Gooddell 4 President', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 4, categoryId: 4, size: 200 }),
-    Channel.create({ name: 'ESPN Sucks', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 7, categoryId: 4, size: 250 }),
-    Channel.create({ name: 'NBA MVP Candidanpmtes', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 3, categoryId: 4, size: 75 }),
+    Channel.create({ name: 'Roger Gooddell 4 President', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 4, categoryId: 5, size: 200 }),
+    Channel.create({ name: 'Cricket is the only sport that\'s good', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 7, categoryId: 5, size: 250 }),
+    Channel.create({ name: 'NBA MVP Candidates', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 3, categoryId: 5, size: 75 }),
 
     Channel.create({ name: 'The Symbolif Motif', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 5, categoryId: 6, size: 200 }),
     Channel.create({ name: 'themes THemes THEMES', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 7,  categoryId: 6, size: 250 }),
@@ -29,8 +28,8 @@ const seedChannels = async function () {
     Channel.create({ name: 'Worst Fantasy Sport', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 9, categoryId: 7, size: 250 }),
     Channel.create({ name: 'What should be next fantasy sport', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 4, categoryId: 7, size: 75 }),
 
-    Channel.create({ name: 'Fatty McGoos', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 4, categoryId: 8, size: 200 }),
-    Channel.create({ name: '820 Good Eatz', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 4, categoryId: 8, size: 250 }),
+    Channel.create({ name: 'Food Truck Fight', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 4, categoryId: 8, size: 200 }),
+    Channel.create({ name: '420 Good Eatz', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 4, categoryId: 8, size: 250 }),
     Channel.create({ name: 'Greasy Meals', type: 'Debate', description: 'BLANK DESCRIPTION', userId: 1, categoryId: 8, size: 75 }),
   ])
   await console.log(`seeded ${channels.length} channels`)
