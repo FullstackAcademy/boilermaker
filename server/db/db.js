74ff54a1b7b34e3dfd928f34ec5aac0d9f31c170
Alejandro Villarreal <avillarreal01@gmail.com>
2018-02-14 20:04:58 -0500

    fix(db): set database name to package name

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,11 @@
 const Sequelize = require('sequelize')
+const pkg = require('../../package.json')
+
+const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
+
 const db = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost:5432/boilermaker', {
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  {
     logging: false
   }
 )
