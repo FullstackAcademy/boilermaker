517aa04b21fe12270160b9d2e63578b1dee3376c
Alejandro Villarreal <avillarreal01@gmail.com>
2018-02-15 07:57:32 -0500

    initial refactor

--- client/components/index.js
+++ client/components/index.js
@@ -3,6 +3,6 @@
  * This way, we can import all of our components from the same place, rather than
  * having to figure out which file they belong to!
  */
-export {default as Main} from './main'
+export {default as Navbar} from './navbar'
 export {default as UserHome} from './user-home'
 export {Login, Signup} from './auth-form'
