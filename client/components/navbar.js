d23a5c6c6a2270d2877e4e32486a13d94e935369
Gabriel Lebec <glebec@gmail.com>
2018-02-26 10:14:34 -0500

    chore(client): remove spaces from import braces

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { logout } from '../store'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+import {logout} from '../store'
 
 const Navbar = ({ handleClick, isLoggedIn }) => (
   <div>
--- client/store/index.js
+++ client/store/index.js
@@ -1,7 +1,7 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux'
 import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
 
 const reducer = combineReducers({user})
