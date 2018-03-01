581414c2ca24e486362ba72a401f83d3f4fac1e5
Aaron Young <brainomite@gmail.com>
2018-02-28 23:55:59 -0500

    webpack-dev-server added

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const isDev = process.env.NODE_ENV === 'development'
+const path = require('path')
 
 module.exports = {
   mode: isDev ? 'development' : 'production',
@@ -7,8 +8,13 @@ module.exports = {
     './client/index.js'
   ],
   output: {
-    path: __dirname,
-    filename: './public/bundle.js'
+    path: path.join(__dirname, '/public'),
+    /*
+      Filename and path, if within filename string below. is relative to the
+      outputPath (defaults to / or root of the site) when using
+      webpack-dev-server.
+     */
+     filename: 'bundle.js',
   },
   devtool: 'source-map',
   module: {
@@ -19,5 +25,26 @@ module.exports = {
         loader: 'babel-loader'
       }
     ]
-  }
+  },
+  devServer: { // Settings related to webpack-dev-server
+    contentBase: path.join(__dirname, '/public'), // Serve your static files from here
+    watchContentBase: true, // Refresh browser if static files change
+    proxy: [ // allows redirect of requests to webpack-dev-server to another destination
+      {
+        context: ['/api', '/auth', '/socket.io'],  // can have multiple, can use wild cards
+        target: 'http://localhost:8080', //server and port to redirect to
+        secure: false,
+      },
+    ],
+    /*
+      ****************    Important     ***********
+      when using the webpack-dev-server, you want to connect to it's port not
+      the nodemon's port.
+    */
+    port: 3000, // port webpack-dev-server listens to, defaults to 8080
+    overlay: { // Shows a full-screen overlay in the browser when there are compiler errors or warnings
+      warnings: false, // defaults to false
+      errors: true, // defaults to false
+    },
+  },
 }
