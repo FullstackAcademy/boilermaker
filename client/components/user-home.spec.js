3878fc4d09c6ef1b6f9071dd8cfa338aab049c2a
Gabriel Lebec <glebec@gmail.com>
2018-03-30 23:48:14 -0400

    feat(prettier): incorporate automatic formatting on commit

--- client/components/user-home.spec.js
+++ client/components/user-home.spec.js
@@ -1,19 +1,19 @@
 /* global describe beforeEach it */
 
-import {expect} from 'chai'
+import { expect } from 'chai'
 import React from 'react'
-import enzyme, {shallow} from 'enzyme'
+import enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import {UserHome} from './user-home'
+import { UserHome } from './user-home'
 
 const adapter = new Adapter()
-enzyme.configure({adapter})
+enzyme.configure({ adapter })
 
 describe('UserHome', () => {
   let userHome
 
   beforeEach(() => {
-    userHome = shallow(<UserHome email={'cody@email.com'} />)
+    userHome = shallow(<UserHome email="cody@email.com" />)
   })
 
   it('renders the email in an h3', () => {
