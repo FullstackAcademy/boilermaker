1af752236bc60f30b11d93efa31b994d6359ef1d
Gabriel Lebec <glebec@gmail.com>
2018-04-08 16:38:18 -0400

    feat(test): seed script sniff test

---
+++ script/seed.spec.js
@@ -0,0 +1,17 @@
+'use strict'
+/* global describe beforeEach it */
+
+const { dim } = require('chalk')
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+
+describe('seed script', () => {
+	it('completes with 0 (successful) exit code', async () => {
+		const { stderr } = await exec('node ./seed', {
+			cwd: __dirname,
+      env: process.env,
+      timeout: 10e3 // 20 secs
+    })
+    console.log(dim('Seed script exited successfully but with stderr:', stderr))
+	})
+})
