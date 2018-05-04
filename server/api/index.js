d4d50ae271c06c8640ebf4dd089bdd63a55a9c69
Finn Terdal <fterdal@gmail.com>
2018-05-04 12:28:03 -0500

    Revert "adds some cats"
    This reverts commit e306c5a4e6893435a6f8c817d44bf2118e3c4481.

--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,6 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
-router.use('/cats', require('./cats'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
