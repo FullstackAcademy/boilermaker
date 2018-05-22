45020479056f1252cf9e62a019e0e19331c5b984
Gabriel Lebec <glebec@gmail.com>
2018-05-22 12:45:25 -0400

    chore(prettier): prettify merged files (seed.js)

--- script/seed.js
+++ script/seed.js
@@ -35,15 +35,16 @@ async function seed() {
 // any errors that might occur inside of `seed`.
 if (module === require.main) {
   seed()
-  .catch(err => {
-    console.error(err)
-    process.exitCode = 1
-  })
-  .finally(() => { // `finally` is like then + catch. It runs no matter what.
-    console.log('closing db connection')
-    db.close()
-    console.log('db connection closed')
-  })
+    .catch(err => {
+      console.error(err)
+      process.exitCode = 1
+    })
+    .finally(() => {
+      // `finally` is like then + catch. It runs no matter what.
+      console.log('closing db connection')
+      db.close()
+      console.log('db connection closed')
+    })
   /*
    * note: everything outside of the async function is totally synchronous
    * The console.log below will occur before any of the logs that occur inside
