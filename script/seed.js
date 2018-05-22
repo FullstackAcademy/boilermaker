55a867d577ce451e90c7b7216b53184d18334786
Gabriel Lebec <glebec@gmail.com>
2018-05-22 12:04:31 -0400

    feat(seed): use seed script directly in spec

--- script/seed.js
+++ script/seed.js
@@ -1,3 +1,8 @@
+'use strict'
+
+const db = require('../server/db')
+const {User} = require('../server/db/models')
+
 /**
  * Welcome to the seed file! This seed file uses a newer language feature called...
  *
@@ -9,15 +14,12 @@
  *
  * Now that you've got the main idea, check it out in practice below!
  */
-const db = require('../server/db')
-const {User} = require('../server/db/models')
 
 async function seed () {
   await db.sync({force: true})
   console.log('db synced!')
   // Whoa! Because we `await` the promise that db.sync returns, the next line will not be
   // executed until that promise resolves!
-
   const users = await Promise.all([
     User.create({email: 'cody@email.com', password: '123'}),
     User.create({email: 'murphy@email.com', password: '123'})
@@ -28,24 +30,27 @@ async function seed () {
   console.log(`seeded successfully`)
 }
 
-// Execute the `seed` function
-// `Async` functions always return a promise, so we can use `catch` to handle any errors
-// that might occur inside of `seed`
-seed()
+// Execute the `seed` function, IF we ran this module directly (`node seed`).
+// `Async` functions always return a promise, so we can use `catch` to handle
+// any errors that might occur inside of `seed`.
+if (module === require.main) {
+  seed()
   .catch(err => {
-    console.error(err.message)
-    console.error(err.stack)
+    console.error(err)
     process.exitCode = 1
   })
-  .then(() => {
+  .finally(() => { // `finally` is like then + catch. It runs no matter what.
     console.log('closing db connection')
     db.close()
     console.log('db connection closed')
   })
+  /*
+   * note: everything outside of the async function is totally synchronous
+   * The console.log below will occur before any of the logs that occur inside
+   * of the async function
+   */
+  console.log('seeding...')
+}
 
-/*
- * note: everything outside of the async function is totally synchronous
- * The console.log below will occur before any of the logs that occur inside
- * of the async function
- */
-console.log('seeding...')
+// we export the seed function for testing purposes (see `./seed.spec.js`)
+module.exports = seed
--- script/seed.spec.js
+++ script/seed.spec.js
@@ -1,17 +1,8 @@
 'use strict'
 /* global describe beforeEach it */
 
-const { dim } = require('chalk')
-const util = require('util')
-const exec = util.promisify(require('child_process').exec)
+const seed = require('./seed')
 
 describe('seed script', () => {
-  it('completes with 0 (successful) exit code', async () => {
-    const { stderr } = await exec('node ./seed', {
-      cwd: __dirname,
-      env: process.env,
-      timeout: 10e3 // 10 secs
-    })
-    console.log(dim('Seed script exited successfully but with stderr:', stderr))
-  })
+  it('completes successfully', seed)
 })
