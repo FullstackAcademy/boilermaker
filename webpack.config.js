73d5bec784ba515e36c493b4e5900ac04765a6f3
Rinaldo <34190720+Rinaldo@users.noreply.github.com>
2018-06-08 17:11:11 -0400

    feat(webpack): support importing jsx files without explicit extension (#100)

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,9 @@ module.exports = {
     path: __dirname,
     filename: './public/bundle.js'
   },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
   devtool: 'source-map',
   module: {
     rules: [
