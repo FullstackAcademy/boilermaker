9279c1279ec77d2c59a15e38d8b81466043f742e
Gabriel Lebec <glebec@gmail.com>
2018-06-19 15:46:16 -0400

    fix(versions, mocha): remove vulnerabilities and deallocate resources (#106)
    * fix(mocha): de-allocate resources properly
    
    * fix(audit): 0 known vulnerabilities

--- server/db/db.js
+++ server/db/db.js
@@ -10,3 +10,9 @@ const db = new Sequelize(
   }
 )
 module.exports = db
+
+// This is a global Mocha hook used for resource cleanup.
+// Otherwise, Mocha v4+ does not exit after tests.
+if (process.env.NODE_ENV === 'test') {
+  after('close database connection', () => db.close())
+}
