6207776b17bcd60b186d14870a4fe38a4cafc0b7
Gabriel Lebec <glebec@gmail.com>
2018-07-21 13:02:07 -0400

    fix(login): restore missing error handling

--- server/auth/index.js
+++ server/auth/index.js
@@ -3,15 +3,19 @@ const User = require('../db/models/user')
 module.exports = router
 
 router.post('/login', async (req, res, next) => {
-  const user = await User.findOne({where: {email: req.body.email}})
-  if (!user) {
-    console.log('No such user found:', req.body.email)
-    res.status(401).send('Wrong username and/or password')
-  } else if (!user.correctPassword(req.body.password)) {
-    console.log('Incorrect password for user:', req.body.email)
-    res.status(401).send('Wrong username and/or password')
-  } else {
-    req.login(user, err => (err ? next(err) : res.json(user)))
+  try {
+    const user = await User.findOne({where: {email: req.body.email}})
+    if (!user) {
+      console.log('No such user found:', req.body.email)
+      res.status(401).send('Wrong username and/or password')
+    } else if (!user.correctPassword(req.body.password)) {
+      console.log('Incorrect password for user:', req.body.email)
+      res.status(401).send('Wrong username and/or password')
+    } else {
+      req.login(user, err => (err ? next(err) : res.json(user)))
+    }
+  } catch (err) {
+    next(err)
   }
 })
 
