ac4b3174a1bb151e677e4ff4755c56957ef82087
mina-gh <minamail@gmail.com>
2018-09-21 10:37:12 -0400

    working on api routes

--- server/api/classes.js
+++ server/api/classes.js
@@ -1,7 +1,51 @@
 const router = require('express').Router();
-const {Class} = require('../db/models');
+const {Class, Lecture} = require('../db/models');
 module.exports = router;
 
-router.get('/', async(req, res, next) => {
+router.get('/:classId', async(req, res, next) => {
+  const classId = req.params.classId;
+  try{
+    // TODO: user privilege check
 
+    const classFound = await Class.findById(classId);
+    if(!classFound){
+      res.status(404).send("Not Found");
+      return;
+    }
+
+    return res.status(200).json(classFound);
+  }catch(err){
+    next(err);
+  }
+});
+
+
+router.get('/:classId/lectures/:lectureId', async(req, res, next) => {
+  const classId = req.params.classId;
+  const lectureId = req.params.lectureId;
+
+  try{
+    // TODO: user privilege check
+
+    const classFound = await Class.findById(classId, {include: Lecture});
+    if(!classFound){
+      res.status(404).send('Not Found');
+      return;
+    }
+
+    let lectureFound;
+    for(let lecture of classFound.lectures){
+      if(lecture.id === lectureId){
+        lectureFound = lecture;
+      }
+    }
+
+    if(!lectureFound){
+      res.status(404).send('Lecture Found');
+    }
+
+    return res.status(200).json(lectureFound);
+  }catch(err){
+    next(err);
+  };
 });
--- server/api/users.js
+++ server/api/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {User} = require('../db/models')
+const {User, Class} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
