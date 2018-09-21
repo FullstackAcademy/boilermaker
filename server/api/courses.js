82499a5d9e8da0edee6be86c6428b1b714e9ccde
mina-gh <minamail@gmail.com>
2018-09-21 15:12:37 -0400

    working on courses and lectures api routes

--- server/api/courses.js
+++ server/api/courses.js
@@ -33,12 +33,18 @@ router.put('/:courseId', async (req, res, next) => {
       res.status(403).send('Forbidden');
     }
 
-    const courseBody = {
-      name: req.body.name,
-      userId: req.body.userId,
+    const tempCourse = await Course.findById(req.params.courseId);
+    if(!tempCourse){
+      res.status(404).send();
+      return;
     }
 
-    if(req.user.admin || req.user.id === Number(req.params.userId)){
+    if(req.user.admin || Number(req.user.id) === req.tempCourse.userId){
+      const courseBody = {
+        name: req.body.name,
+        userId: req.body.userId,
+      };
+
       const course = await Course.update(courseBody, {
         where: {
           id: req.params.courseId,
@@ -57,3 +63,29 @@ router.put('/:courseId', async (req, res, next) => {
     next(err);
   }
 })
+
+
+// DELETE /api/courses/:courseId
+router.delete('/courses/:courseId', async (req, res, next) => {
+  const courseId = req.params.courseId;
+  try{
+    const course = await Course.findById(courseId);
+    if(!course){
+      res.status(404).send("Class Not Found");
+      return;
+    }
+
+    if(!req.user.admin && req.user.id !== course.userId){
+      res.status(403).send('Forbidden');
+      return;
+    }
+
+    await Course.destroy({
+      where: {id: courseId}
+    })
+
+    res.status(201).send();
+  }catch(err){
+    next(err);
+  }
+});
--- server/api/lectures.js
+++ server/api/lectures.js
@@ -2,6 +2,86 @@ const router = require('express').Router();
 const {Lecture} = require('../db/models');
 module.exports = router;
 
-router.get('/', async(req, res, next) => {
+// POST
+router.post('/', async(req, res, next) => {
+  try{
+    const {title, youtube_url, note, userId, courseId} = req.body;
 
+    let lecture = await Lecture.findOne({
+      where: {
+        youtube_url, userId
+      }
+    });
+
+  }catch(err){
+    next(err);
+  }
+});
+
+// PUT
+router.put('/:lectureId', async(req, res, next) => {
+  const lectureId = req.params.lectureId;
+
+  try{
+    if(!req.user || !req.user.admin){
+      res.status(403).send('Forbidden');
+    }
+
+    const lectureBody = {
+      name: req.body.title,
+      youtube_url: req.body.youtube_url,
+      note: req.body.note,
+      userId: req.body.userId,
+      courseId: req.body.courseId,
+    }
+
+    const tempLecture = await Lecture.findById(lectureId);
+    if(!tempLecture){
+      res.status(404).send();
+      return;
+    }
+
+    if(req.user.admin || req.user.id === req.tempLecture.userId){
+      const lecture = await Lecture.update(lectureBody, {
+        where: {
+          id: req.params.lecture,
+        },
+        returning: true,
+      });
+
+      if(!lecture){
+        res.status(404).send('Course Not Found');
+        return;
+      }else{
+        res.json(lecture);
+      }
+    }
+  }catch(err){
+    next(err);
+  }
+});
+
+// DELETE
+router.delete('/lectures/:lectureId', async (req, res, next) => {
+  const lectureId = req.params.lectureId;
+  try{
+    const lecture = await Lecture.findById(lectureId);
+    if(!lecture){
+      res.status(404).send("Lecture Not Found");
+      return;
+    }
+
+    if(!req.user.admin && req.user.id !== lecture.userId){
+      res.status(403).send('Forbidden');
+      return;
+    }
+
+    await Lecture.destroy({
+      where: {id: lectureId}
+    })
+
+    res.status(201).send();
+  }catch(err){
+    next(err);
+  }
 });
