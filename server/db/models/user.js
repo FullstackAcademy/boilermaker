d962680337c972d6b0ff94b2fe43cca192159144
Adam Zhou <adzhou218@gmail.com>
2018-10-29 18:15:28 -0400

    branch check

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto')
 const Sequelize = require('sequelize')
 const db = require('../db')
-
+//newbranch branch
 const User = db.define('user', {
   email: {
     type: Sequelize.STRING,
