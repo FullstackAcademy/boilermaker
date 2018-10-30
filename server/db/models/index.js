befaf6b3139c2c7c77d6395f7ce81537cfb01057
Dan Needham <dan.needham3@gmail.com>
2018-10-30 12:25:05 -0400

    cart model and associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,11 +1,14 @@
+const db = require('../db')
 const User = require('./user')
-
+const Cart = require('./cart')
+const Product = require('./product')
+const CartProducts = require('./cartProducts')
 /**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
+ * associations
  */
+
+Cart.belongsToMany(Product, { through: CartProducts })
+Product.belongsToMany(Cart, { through: CartProducts })
 
 /**
  * We'll export all of our models here, so that any time a module needs a model,
@@ -14,5 +17,9 @@ const User = require('./user')
  * instead of: const User = require('../db/models/user')
  */
 module.exports = {
-  User
+  db,
+  User,
+  Product,
+  Cart,
+  CartProducts
 }
