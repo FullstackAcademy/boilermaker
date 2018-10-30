207138e2d6ad739d21174aca716af80954640f9c
Dan Needham <dan.needham3@gmail.com>
2018-10-30 12:04:49 -0400

    initial cart products assoc table

---
+++ server/db/models/cartProducts.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const CartProduct = db.define('cartProduct' {
+  quantity: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0
+  }
+})
+
+module.exports = CartProduct
