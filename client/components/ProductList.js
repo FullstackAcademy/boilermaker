3e43b594e5655f20946bcbb1c131e12410c743f9
Adam Zhou <adzhou218@gmail.com>
2018-10-31 18:08:32 -0400

    old master

--- client/components/ProductList.js
+++ client/components/ProductList.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {getProductsThunk} from '../store/products'
+import axios from 'axios'
 
 class ProductList extends Component {
   constructor(props) {
@@ -10,6 +11,22 @@ class ProductList extends Component {
   componentDidMount() {
     this.props.getProducts()
   }
+
+  async addToCart(productId) {
+    try {
+      const newCartResponse = await axios.post('/api/carts', {})
+      const newCart = newCartResponse.data
+      const newProductInCartResponse = await axios.post('api/cartProducts', {
+        productId,
+        cartId: newCart.id,
+        quantity: 1
+      })
+      const newProductInCart = newProductInCartResponse.data
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   render() {
     return (
       <div className="product-list">
@@ -25,6 +42,9 @@ class ProductList extends Component {
               <div>
                 <img className="product-img" src={product.imageUrl} />
               </div>
+              <button onClick={() => this.addToCart(product.id)}>
+                Add to cart
+              </button>
             </div>
           )
         })}
@@ -34,7 +54,7 @@ class ProductList extends Component {
 }
 
 const mapStateToProps = state => ({
-  products: state.products.products
+  products: state.products
 })
 
 const mapDispatchToProps = dispatch => ({
--- server/db/models/cart.js
+++ server/db/models/cart.js
@@ -1,6 +1,12 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
 //add test
-const Cart = db.define('cart', {})
+const Cart = db.define('cart', {
+  purchased: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+    allowNull: false
+  }
+})
 
 module.exports = Cart
