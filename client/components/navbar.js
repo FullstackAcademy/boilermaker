98e47de947eed0dcf0930c4ab8d65d2f89fc11b6
Adam Zhou <adzhou218@gmail.com>
2018-10-31 15:47:14 -0400

    removed material-ui

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,43 +3,36 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
-import Button from '@material-ui/core/Button'
-import AppBar from '@material-ui/core/AppBar'
-import ToolBar from '@material-ui/core/ToolBar'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div className="header-nav">
     <h1>Iron Flown Grocery</h1>
-    <AppBar position="static">
-      <ToolBar className="navbar">
-        <nav>
-          {isLoggedIn ? (
-            <div>
-              {/* The navbar will show these links after you log in */}
-              <Link to="/home">Home</Link>
-              <a href="#" onClick={handleClick}>
-                Logout
-              </a>
-            </div>
-          ) : (
-            <div>
-              {/* The navbar will show these links before you log in */}
-              <Button component={Link} to="/login">
-                Login
-              </Button>
-              <Button component={Link} to="/signup">
-                Signup
-              </Button>
-              <Button component={Link} to="/addproduct">
-                Add Product
-              </Button>
-              <Button>Cart</Button>
-            </div>
-          )}
-        </nav>
-        <hr />
-      </ToolBar>
-    </AppBar>
+    <nav>
+      {isLoggedIn ? (
+        <div>
+          {/* The navbar will show these links after you log in */}
+          <Link to="/home">Home</Link>
+          <a href="#" onClick={handleClick}>
+            Logout
+          </a>
+        </div>
+      ) : (
+        <div>
+          {/* The navbar will show these links before you log in */}
+          <Button component={Link} to="/login">
+            Login
+          </Button>
+          <Button component={Link} to="/signup">
+            Signup
+          </Button>
+          <Button component={Link} to="/addproduct">
+            Add Product
+          </Button>
+          <Button>Cart</Button>
+        </div>
+      )}
+    </nav>
+    <hr />
   </div>
 )
 
