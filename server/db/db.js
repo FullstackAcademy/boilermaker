bb1a435bb06065be71ba49c4f51e74c26b02c51b
Jeb Roberts <jebroberts2@gmail.com>
2018-10-31 11:43:29 -0400

    Db name changed

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
 
-const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
+const databaseName = pkg.name
+// + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
