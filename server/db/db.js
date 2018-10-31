ceffbe189fc4166c1cd41028ab1945cdee29dd4c
Jeb Roberts <jebroberts2@gmail.com>
2018-10-31 12:11:52 -0400

    local db removed

--- server/db/db.js
+++ server/db/db.js
@@ -1,11 +1,11 @@
 const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
 
-// const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
+const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
 const db = new Sequelize(
-  process.env.DATABASE_URL ||
-    `postgres://localhost:5432/postgresql-trapezoidal-29972`,
+  process.env.DATABASE_URL,
+  // || `postgres://localhost:5432/${databaseName}`,
   {
     logging: false
   }
