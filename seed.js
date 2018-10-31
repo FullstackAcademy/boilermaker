7d2620ac0c74d2b6a9907910452899c16ff5ca36
Jeb Roberts <jebroberts2@gmail.com>
2018-10-31 11:06:33 -0400

    Add a plum

--- seed.js
+++ seed.js
@@ -1,84 +1,85 @@
-const {
-  green,
-  red
-} = require('chalk');
+const {green, red} = require('chalk')
 const {
   db,
   User,
   Product,
   Cart,
   CartProducts
-} = require('./server/db/models/index');
+} = require('./server/db/models/index')
 
-const users = [{
-    firstName: "Bob",
-    lastName: "Builder",
-    email: "bob.builder@fullstack.com",
-    password: "AlwaysBeCoding"
+const users = [
+  {
+    firstName: 'Bob',
+    lastName: 'Builder',
+    email: 'bob.builder@fullstack.com',
+    password: 'AlwaysBeCoding'
   },
   {
-    firstName: "Bill",
-    lastName: "Nye",
-    email: "Bill.Nye@fullstack.com",
-    password: "BILLBILLBILL"
+    firstName: 'Bill',
+    lastName: 'Nye',
+    email: 'Bill.Nye@fullstack.com',
+    password: 'BILLBILLBILL'
   },
   {
-    firstName: "Arthur",
-    lastName: "Read",
-    email: "arthur.read@fullstack.com",
-    password: "AndISayHey"
-  }, {
-    firstName: "Curios",
-    lastName: "George",
-    email: "curios.george@fullstack.com",
-    password: "GetDownFromThatTree"
+    firstName: 'Arthur',
+    lastName: 'Read',
+    email: 'arthur.read@fullstack.com',
+    password: 'AndISayHey'
+  },
+  {
+    firstName: 'Curios',
+    lastName: 'George',
+    email: 'curios.george@fullstack.com',
+    password: 'GetDownFromThatTree'
   }
 ]
 
-
-const products = [{
-    name: "apple",
+const products = [
+  {
+    name: 'apple',
     price: 1
   },
   {
-    name: "avacado",
+    name: 'avacado',
     price: 3
   },
   {
-    name: "banana",
+    name: 'banana',
     price: 2
   },
   {
-    name: "strawberry",
+    name: 'strawberry',
     price: 5
+  },
+  {
+    name: 'plum',
+    price: 4
   }
 ]
-
-
 
 const seed = async () => {
   await db.sync({
     force: true
-  });
+  })
 
   // seed your database here!
   await Promise.all(
     products.map(product => {
-      Product.create(product);
+      Product.create(product)
     })
-  );
+  )
   await Promise.all(
     users.map(user => {
-      User.create(user);
+      User.create(user)
     })
-  );
+  )
 
-  console.log(green('Seeding success!'));
+  console.log(green('Seeding success!'))
   // db.close();
-};
+}
 
 seed().catch(err => {
-  console.error(red('Oh noes! Something went wrong!'));
-  console.error(err);
+  console.error(red('Oh noes! Something went wrong!'))
+  console.error(err)
   // db.close();
 })
