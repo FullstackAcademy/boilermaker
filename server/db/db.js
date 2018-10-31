008bfc5738e9bdbb7af41b9eb94b427f647582d1
Jeb Roberts <jebroberts2@gmail.com>
2018-10-31 12:17:41 -0400

    Local db added back

--- server/db/db.js
+++ server/db/db.js
@@ -4,8 +4,7 @@ const pkg = require('../../package.json')
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
 const db = new Sequelize(
-  process.env.DATABASE_URL,
-  // || `postgres://localhost:5432/${databaseName}`,
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
     logging: false
   }
