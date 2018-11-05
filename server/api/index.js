635911f80ec0c45011eb87bd664cc3f751ccf415
Jeb Roberts <jebroberts2@gmail.com>
2018-11-05 18:27:24 -0500

    total price displayed on cart

--- server/api/index.js
+++ server/api/index.js
@@ -5,7 +5,7 @@ router.use('/users', require('./users_route'))
 router.use('/products', require('./products_route'))
 router.use('/carts', require('./carts_route'))
 router.use('/cartProducts', require('./cartProducts_route'))
-router.use('/stripe', require('./stripe_route'))
+// router.use('/stripe', require('./stripe_route'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
