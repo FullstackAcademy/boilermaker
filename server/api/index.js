0f0ebf163b75d694157aa9c2c9b7a9f3a7cb2d2c
Jeb Roberts <jebroberts2@gmail.com>
2018-11-05 10:44:53 -0500

    refactoring names of files

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router()
 module.exports = router
 
-router.use('/users', require('./users'))
-router.use('/products', require('./products'))
-router.use('/carts', require('./carts'))
-router.use('/cartProducts', require('./cartProducts'))
+router.use('/users', require('./users_route'))
+router.use('/products', require('./products_route'))
+router.use('/carts', require('./carts_route'))
+router.use('/cartProducts', require('./cartProducts_route'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
