eeaa34a37b69c1d17ee458cfde978f274456c927
Finn <finnterdal@gmail.com>
2019-05-21 13:26:45 -0500

    oops, forgot to run prettier on it

--- client/history.js
+++ client/history.js
@@ -1,6 +1,8 @@
 import {createMemoryHistory, createBrowserHistory} from 'history'
 
 const history =
-  process.env.NODE_ENV === 'test' ? createMemoryHistory() : createBrowserHistory()
+  process.env.NODE_ENV === 'test'
+    ? createMemoryHistory()
+    : createBrowserHistory()
 
 export default history
