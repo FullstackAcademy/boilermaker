83791fdd74c2818fba480d845ee2d9efb4c9a66a
Tal Luigi <luigi.t.agr@gmail.com>
2019-10-16 09:23:25 -0400

    update: changed console.logs text and coloring in the seed file

--- script/seed.js
+++ script/seed.js
@@ -2,34 +2,36 @@
 
 const db = require('../server/db')
 const {User} = require('../server/db/models')
+const {red, green} = require('chalk')
 
 async function seed() {
   await db.sync({force: true})
-  console.log('db synced!')
+  console.log('Database is synced!')
 
   const users = await Promise.all([
     User.create({email: 'cody@email.com', password: '123'}),
     User.create({email: 'murphy@email.com', password: '123'})
   ])
 
-  console.log(`seeded ${users.length} users`)
-  console.log(`seeded successfully`)
+  console.log(`Seeded ${users.length} Users`)
+  console.log(green('Seeded database successfully'))
 }
 
 // We've separated the `seed` function from the `runSeed` function.
 // This way we can isolate the error handling and exit trapping.
 // The `seed` function is concerned only with modifying the database.
 async function runSeed() {
-  console.log('seeding...')
+  console.log('Seeding...')
   try {
     await seed()
   } catch (err) {
+    console.error(red('Oh no! Something went wrong!'))
     console.error(err)
     process.exitCode = 1
   } finally {
-    console.log('closing db connection')
+    console.log('Closing database connection...')
     await db.close()
-    console.log('db connection closed')
+    console.log('Database connection is closed!')
   }
 }
 
