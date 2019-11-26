e448f9f68a8f67cdc7852c2802f57039b5ca3aaf
EthanG19 <eth13gou2514@gmail.com>
2019-11-26 14:07:16 -0500

    css

--- env/lib/python3.7/site-packages/gast/__init__.py
+++
@@ -1,4 +0,0 @@
-from .gast import *
-from ast import (NodeVisitor, NodeTransformer, fix_missing_locations,
-                 copy_location, increment_lineno,
-                 iter_child_nodes, iter_fields, walk, dump)
--- env/lib/python3.7/site-packages/tensorflow_estimator-2.0.1.dist-info/DESCRIPTION.rst
+++
@@ -1,4 +0,0 @@
-TensorFlow Estimator is a high-level API that encapsulates model training,
-evaluation, prediction, and exporting.
-
-
