4c51763b67f1ebd09f55d85be73459758427244c
EthanG19 <eth13gou2514@gmail.com>
2019-12-01 18:29:57 -0500

    first commit

--- client/components/Camera.js
+++ client/components/Camera.js
@@ -1,43 +1,59 @@
 import React, {Component} from 'react'
-import Camera from 'react-camera'
+import Webcam from 'react-webcam'
 import {Button, Header, Image, Modal} from 'semantic-ui-react'
-
-class Webcam extends Component {
-  constructor() {
-    super()
-    this.handleClick = this.handleClick.bind(this)
-  }
-  render() {
-    return (
-      <Modal trigger={<Button id="webcam-button">Show Modal</Button>}>
-        <Modal.Header>Submit a Photo</Modal.Header>
-        <Modal.Content image>
-          <Camera
-            ref={cam => {
-              this.camera = cam
-            }}
-          />
-          <img
-            ref={img => {
-              this.img = img
-            }}
-          />
-          <Modal.Description>
-            <Header>Make the displayed hand sign</Header>
-            <p>Please keep the hand used to sign in the box for the photo</p>
-            <p>Is it okay to use this photo?</p>
-            <Button onClick={this.handleClick}>Take Photo</Button>
-          </Modal.Description>
-        </Modal.Content>
-      </Modal>
-    )
-  }
-  handleClick(e) {
-    e.preventDefault()
-    this.camera.capture().then(blob => {
-      this.img.src = URL.createObjectURL(blob)
-    })
-  }
+import axios from 'axios'
+const videoConstraints = {
+  width: 1280,
+  height: 720,
+  facingMode: 'user'
 }
 
-export default Webcam
+const WebcamModal = () => {
+  const webcamRef = React.useRef(null)
+
+  const capture = React.useCallback(
+    async () => {
+      const imgSrc = webcamRef.current.getScreenshot()
+      const config = {
+        headers: {
+          'X-Custom-Header': Math.random(),
+          'Access-Control-Allow-Origin': '*'
+        }
+      }
+      console.log(imgSrc)
+      const res = await axios.post(
+        'http://allsignpython.herokuapp.com/result',
+        {imgSrc},
+        config
+      )
+      console.log(res)
+    },
+    [webcamRef]
+  )
+
+  return (
+    <Modal trigger={<Button id="webcam-button">Show Modal</Button>}>
+      <Modal.Header>Submit a Photo</Modal.Header>
+      <Modal.Content image>
+        <>
+          <Webcam
+            audio={false}
+            height={250}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            width={500}
+            videoConstraints={videoConstraints}
+          />
+        </>
+        <Modal.Description>
+          <Header>Make the displayed hand sign</Header>
+          <p>Please keep the hand used to sign in the box for the photo</p>
+          <p>Is it okay to use this photo?</p>
+          <Button onClick={capture}>Take Photo</Button>
+        </Modal.Description>
+      </Modal.Content>
+    </Modal>
+  )
+}
+
+export default WebcamModal
