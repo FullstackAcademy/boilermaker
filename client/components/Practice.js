c187b98617c05fd55111ac7d1d2208f741bfd9e0
EthanG19 <eth13gou2514@gmail.com>
2019-12-03 12:44:49 -0500

    fix bug

--- client/components/Practice.js
+++ client/components/Practice.js
@@ -1,16 +1,56 @@
 import React, {Component} from 'react'
-import {Navbar} from '.'
-import {Footer} from './footer'
+import {Form, Radio, Segment} from 'semantic-ui-react'
 
-class Practice extends Component {
+export default class Practice extends Component {
+  constructor() {
+    super()
+    this.state = {}
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e, {value}) {
+    this.setState({value})
+  }
+
   render() {
     return (
       <div>
-        <Navbar />
-        <Footer />
+        hello
+        <Form>
+          <Form.Field id="quizform">
+            Selected value: <b>{this.state.value}</b>
+          </Form.Field>
+          <Form.Field>
+            <Radio
+              value="a"
+              checked={this.state.value === 'a'}
+              onChange={this.handleChange}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Radio
+              value="b"
+              checked={this.state.value === 'b'}
+              onChange={this.handleChange}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Radio
+              value="c"
+              checked={this.state.value === 'c'}
+              onChange={this.handleChange}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Radio
+              value="d"
+              checked={this.state.value === 'd'}
+              onChange={this.handleChange}
+            />
+          </Form.Field>
+        </Form>
       </div>
     )
   }
 }
-
-export default Practice
