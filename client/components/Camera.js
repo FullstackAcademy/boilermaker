7afb87398390c2fe72541164cca8ebdccd027119
EthanG19 <eth13gou2514@gmail.com>
2019-12-04 17:01:17 -0500

    final

--- client/components/Camera.js
+++ client/components/Camera.js
@@ -2,58 +2,71 @@ import React, {Component} from 'react'
 import Webcam from 'react-webcam'
 import {Button, Header, Image, Modal} from 'semantic-ui-react'
 import axios from 'axios'
-const videoConstraints = {
-  width: 1280,
-  height: 720,
-  facingMode: 'user'
-}
+import * as tf from '@tensorflow/tfjs'
+import * as tmImage from '@teachablemachine/image'
 
 const WebcamModal = () => {
-  const webcamRef = React.useRef(null)
+  const URL = 'https://teachablemachine.withgoogle.com/models/NeX9xVyz/'
 
-  const capture = React.useCallback(
-    async () => {
-      try {
-        const imgSrc = webcamRef.current.getScreenshot()
-        const config = {
-          headers: {
-            'X-Custom-Header': Math.random(),
-            'Access-Control-Allow-Origin': '*'
-          }
-        }
-        console.log(imgSrc)
-        const res = await axios.post(
-          'https://allsignpython.herokuapp.com/result',
-          {imgSrc},
-          config
-        )
-        console.log(res)
-      } catch (error) {
-        console.log(error)
-      }
-    },
-    [webcamRef]
-  )
+  let model, webcam, labelContainer, maxPredictions
 
+  // Load the image model and setup the webcam
+  async function init() {
+    const modelURL = URL + 'model.json'
+    const metadataURL = URL + 'metadata.json'
+
+    // load the model and metadata
+    // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+    // or files from your local hard drive
+    // Note: the pose library adds "tmImage" object to your window (window.tmImage)
+    model = await tmImage.load(modelURL, metadataURL)
+    maxPredictions = model.getTotalClasses()
+
+    // Convenience function to setup a webcam
+    const flip = true // whether to flip the webcam
+    webcam = new tmImage.Webcam(200, 200, flip) // width, height, flip
+    await webcam.setup() // request access to the webcam
+    await webcam.play()
+    window.requestAnimationFrame(loop)
+
+    // append elements to the DOM
+    document.getElementById('webcam-container').appendChild(webcam.canvas)
+    labelContainer = document.getElementById('label-container')
+    for (let i = 0; i < maxPredictions; i++) {
+      // and class labels
+      labelContainer.appendChild(document.createElement('div'))
+    }
+  }
+
+  async function loop() {
+    webcam.update() // update the webcam frame
+    await predict()
+    window.requestAnimationFrame(loop)
+  }
+
+  // run the webcam image through the image model
+  async function predict() {
+    // predict can take in an image, video or canvas html element
+    const prediction = await model.predict(webcam.canvas)
+    for (let i = 0; i < maxPredictions; i++) {
+      const classPrediction =
+        prediction[i].className + ': ' + prediction[i].probability.toFixed(2)
+      labelContainer.childNodes[i].innerHTML = classPrediction
+    }
+  }
   return (
     <Modal trigger={<Button id="webcam-button">Start</Button>}>
       <Modal.Header>Submit a Photo</Modal.Header>
       <Modal.Content image>
         <>
-          <Webcam
-            audio={false}
-            height={250}
-            ref={webcamRef}
-            screenshotFormat="image/jpeg"
-            width={500}
-            videoConstraints={videoConstraints}
-          />
+          <div id="webcam-container" />
+          <div id="label-container" />
         </>
         <Modal.Description>
           <Header>Make the displayed hand sign</Header>
           <p>Please keep the hand used to sign in the box for the photo</p>
           <p>Is it okay to use this photo?</p>
-          <Button onClick={capture}>Take Photo</Button>
+          <Button onClick={init}>Start</Button>
         </Modal.Description>
       </Modal.Content>
     </Modal>
---
+++ server/api/quizQuestions.js
@@ -0,0 +1,90 @@
+const quizQuestions = [
+  {
+    question: 'What franchise would you rather play a game from?',
+    answers: [
+      {
+        type: 'Microsoft',
+        content: 'Halo'
+      },
+      {
+        type: 'Nintendo',
+        content: 'Pokemon'
+      },
+      {
+        type: 'Sony',
+        content: 'Uncharted'
+      }
+    ]
+  },
+  {
+    question: 'Which console would you prefer to play with friends?',
+    answers: [
+      {
+        type: 'Microsoft',
+        content: 'X-Box'
+      },
+      {
+        type: 'Nintendo',
+        content: 'Nintendo 64'
+      },
+      {
+        type: 'Sony',
+        content: 'Playstation 1'
+      }
+    ]
+  },
+  {
+    question:
+      'Which of these racing franchises would you prefer to play a game from?',
+    answers: [
+      {
+        type: 'Microsoft',
+        content: 'Forza'
+      },
+      {
+        type: 'Nintendo',
+        content: 'Mario Kart'
+      },
+      {
+        type: 'Sony',
+        content: 'Gran Turismo'
+      }
+    ]
+  },
+  {
+    question: 'Which of these games do you think is best?',
+    answers: [
+      {
+        type: 'Microsoft',
+        content: 'BioShock'
+      },
+      {
+        type: 'Nintendo',
+        content: 'The Legend of Zelda: Ocarina of Time'
+      },
+      {
+        type: 'Sony',
+        content: 'Final Fantasy VII'
+      }
+    ]
+  },
+  {
+    question: 'What console would you prefer to own?',
+    answers: [
+      {
+        type: 'Microsoft',
+        content: 'X-Box One'
+      },
+      {
+        type: 'Nintendo',
+        content: 'Wii U'
+      },
+      {
+        type: 'Sony',
+        content: 'Playstation 4'
+      }
+    ]
+  }
+]
+
+export default quizQuestions
