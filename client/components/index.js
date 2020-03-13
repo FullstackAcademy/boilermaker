2a457f13502eac9fefe12681fae25cd61990bed7
Natasha Whitney <natiwhitney@gmail.com>
2020-03-13 11:43:52 -0400

    Import Map component to routes, navbar

--- client/components/index.js
+++ client/components/index.js
@@ -5,4 +5,5 @@
  */
 export {default as Navbar} from './navbar'
 export {default as UserHome} from './user-home'
+export {default as Map} from './map'
 export {Login, Signup} from './auth-form'
--- client/components/navbar.js
+++ client/components/navbar.js
@@ -12,6 +12,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
         <div>
           {/* The navbar will show these links after you log in */}
           <Link to="/home">Home</Link>
+          <Link to="/map">Map</Link>
           <a href="#" onClick={handleClick}>
             Logout
           </a>
@@ -19,6 +20,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
       ) : (
         <div>
           {/* The navbar will show these links before you log in */}
+          <Link to="/map">Map</Link>
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
         </div>
