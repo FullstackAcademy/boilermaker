6aae8e82f3a3ed13cfe09e10c3a943c21105c4e4
Natasha Whitney <natiwhitney@gmail.com>
2020-03-13 11:44:24 -0400

    Add mapbox auth to store reducer

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,9 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
+import authToken from './mapbox'
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({user, authToken})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
@@ -12,3 +13,4 @@ const store = createStore(reducer, middleware)
 
 export default store
 export * from './user'
+export * from './mapbox'
