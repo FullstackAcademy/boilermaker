c8a8424302278332e372a2d51999972cf9dbfc93
Travis Stratton <t.strat7@gmail.com>
2020-12-26 22:17:26 -0600

    removed unused vars

--- server/index.js
+++ server/index.js
@@ -95,9 +95,7 @@ const createApp = () => {
 
 const startListening = () => {
   // start listening (and create a 'server' object representing our server)
-  const server = app.listen(PORT, () =>
-    console.log(`Mixing it up on port ${PORT}`)
-  )
+  app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
 }
 
 const syncDb = () => db.sync()
