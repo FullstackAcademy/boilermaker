c83433a10843b3d1c0a50d6a6dc97c0d52a7a26d
nlane <nlane@udel.edu>
2021-02-24 11:14:56 -0500

    removes some config values

--- server/db/db.js
+++ server/db/db.js
@@ -8,9 +8,6 @@ let config
 if (process.env.DATABASE_URL) {
   config = {
     logging: false,
-    operatorsAliases: false,
-    dialect: 'postgres',
-    protocol: 'postgres',
     ssl: true,
     dialectOptions: {
       ssl: {
