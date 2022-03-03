2ba4be1560cfd56b22400f1db0e4277f072251ef
David Dunham <dd567@cornell.edu>
2022-03-03 16:32:05 -0500

    working on fixing merge

--- client/app.js
+++ client/app.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 import {Navbar} from './components'
 import Routes from './routes'
-import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
   return (
     <div>
-      {/* <Navbar /> */}
+      <Navbar />
       <Routes />
     </div>
   )
