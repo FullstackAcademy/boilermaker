0b70fea172828a1e035da1ad3a632fb54c088257
David Dunham <dd567@cornell.edu>
2022-03-04 17:03:21 -0500

    added icons to links on navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -5,6 +5,11 @@ import {Link} from 'react-router-dom'
 import {logout} from '../store'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import Badge from '@mui/material/Badge';
+import LocalPizzaIcon from '@mui/icons-material/LocalPizza';
+import HomeIcon from '@mui/icons-material/Home';
+import LogoutIcon from '@mui/icons-material/Logout';
+import LoginIcon from '@mui/icons-material/Login';
+import AirplaneTicketIcon from '@mui/icons-material/AirplaneTicket';
 
 const Navbar = ({handleClick, isLoggedIn, cartSize}) => (
   <div id="nav">
@@ -12,36 +17,44 @@ const Navbar = ({handleClick, isLoggedIn, cartSize}) => (
       {isLoggedIn ? (
         <div>
           <Link className="navLink" to="/userhome">
+            <HomeIcon />
             Home
           </Link>
           <Link className="navLink" to="/pizzas">
+            <LocalPizzaIcon />
             Pizzas
           </Link>
           <Link className="navLink" to="/checkout">
             testCheckOut
           </Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
           <Link className="navLink" to="/cart">
             <Badge badgeContent={cartSize} color="success" >
               <ShoppingCartIcon color="primary" />
             </Badge>
+            Cart
           </Link>
+          <a href="#" onClick={handleClick}>
+            <LogoutIcon />
+            Logout
+          </a>
         </div>
       ) : (
         <div>
           {/* The navbar will show these links before you log in */}
           <Link className="navLink" to="/home">
+            <HomeIcon />
             Home
           </Link>
           <Link className="navLink" to="/pizzas">
+            <LocalPizzaIcon />
             Pizzas
           </Link>
           <Link className="navLink" to="/login">
+            <LoginIcon />
             Login
           </Link>
           <Link className="navLink" to="/signup">
+            <AirplaneTicketIcon />
             Sign Up
           </Link>
         </div>
