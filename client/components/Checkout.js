7f51d49499b838ae8a87d12502a3d5e51056e26d
Brennan Skinner <bgskinner3@gmail.com>
2022-03-04 14:38:33 -0500

    checkout cart updated

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -2,68 +2,158 @@ import React from "react";
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
-
+import { DropdownButton, Dropdown, Row, Col, FloatingLabel } from "react-bootstrap";
 
 
 class Checkout extends React.Component {
 
   render() {
   return (
+    <div>
     <div className="shippingaddress">
       <Form>
+        <Form.Label>Shipping Address</Form.Label>
         <Form.Group>
-          <Form.Label>Email address</Form.Label>
+          <FloatingLabel label="Email address"> 
             <Form.Control
               name="email"
               type="text"
               placeholder="Enter email"
               required
             />
+            </FloatingLabel>
         </Form.Group>
+
          <Form.Group>
-          <Form.Label>
-            Street Address
-          </Form.Label>
+          <FloatingLabel label="Street Address">
+           
+        
           <Form.Control 
           name="streetaddress"
           type="text"
           placeholder="Enter you Address"
+          required
           />
+            </FloatingLabel>
         </Form.Group>
+        <Row>
+          <Col>
         <Form.Group>
-          <Form.Label>
-           Postal/Zip Code
-          </Form.Label>
+          <FloatingLabel label="Postal/Zip Code">
           <Form.Control 
           name="postal/zip/code"
           type="text"
-          placeholder="Postal/Zip Code*"
+          placeholder="Postal/Zip Code"
+          required
           />
+          </FloatingLabel>
         </Form.Group>
+        </Col>
+
+        <Col>
         <Form.Group>
-          <Form.Label>
-            City
-          </Form.Label>
+          <FloatingLabel label="City">
+            
+          
           <Form.Control 
           name="city"
           type="text"
-          placeholder="City*"
+          placeholder="City"
+          required
           />
+          </FloatingLabel>
         </Form.Group>
+        </Col>
+
+
+        <Col>
         <Form.Group>
-          <Form.Label>
-           State
-          </Form.Label>
+          <FloatingLabel label="State">
+        
+          
           <Form.Control 
-          name="State"
+          name="state"
           type="text"
           placeholder="Province/State"
+          required
           />
+          </FloatingLabel>
         </Form.Group>
-       
+        </Col>
+        </Row>
+        <Button variant="primary" type="submit">
+            Save
+          </Button>
+      </Form>
+      </div>
 
+      <div>
+      <Form title="Payment Informtion">
+         <Form.Label>Payment Information</Form.Label>
+        <Row>
+          <Col>
+        <Form.Group>
+          <FloatingLabel label="Name on Card">
+            <Form.Control
+              name="cardname"
+              type="text"
+              placeholder="Name on Card"
+              required
+            />
+            </FloatingLabel>
+        </Form.Group>
+        </Col>
+        <Col>
+        <DropdownButton id="dropdown-basic-button" title="Card">
+          <Dropdown.Item>Visa</Dropdown.Item>
+          <Dropdown.Item>MasterCard</Dropdown.Item>
+          <Dropdown.Item>Amex</Dropdown.Item>
+        </DropdownButton>
+        </Col>
+        </Row>
+
+        <Form.Group>
+          <FloatingLabel label="Credit Card No.">
+            <Form.Control
+              name="cardnumber"
+              type="number"
+              placeholder="Credit Card No."
+              required
+            />
+            </FloatingLabel>
+        </Form.Group>
+        <Row>
+          <Col>
+        <Form.Group>
+          <FloatingLabel label="CVC">
+            <Form.Control
+              name="cvc"
+              type="text"
+              placeholder="CVC"
+              required
+            />
+            </FloatingLabel>
+        </Form.Group>
+        </Col>
+        <Col>
+        <Form.Group>
+          <FloatingLabel label="Exp. Date">
+            <Form.Control
+              name="exp"
+              type="Date"
+              placeholder="Exp."
+              required
+            />
+            </FloatingLabel>
+        </Form.Group>
+        </Col>
+        </Row>
+        <Button variant="primary" type="submit">
+            Save
+          </Button>
       </Form>
       
+    </div>
     </div>
   )
   }
