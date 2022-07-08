6e6095f240963bd60feec90e28f0c1e2e167c4be
Chiara Marcial Martínez <contact@cofuente.io>
2022-07-08 17:12:52 -0400

    minor change
    Signed-off-by: Chiara Marcial Martínez <contact@cofuente.io>

--- script/seed.js
+++ script/seed.js
@@ -8,8 +8,8 @@ async function seed() {
   console.log('db synced!')
 
   const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
+    User.create({email: 'test-one@email.com', password: '123'}),
+    User.create({email: 'test-two@email.com', password: '123'})
   ])
 
   console.log(`seeded ${users.length} users`)
