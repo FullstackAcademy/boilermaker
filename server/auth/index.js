82c1d4dd28760a3a7f9c7e90e0da84ea27bab7b8
Chiara Marcial Martínez <contact@cofuente.io>
2022-07-08 23:46:18 -0400

    add eslint rule: no-console
    Signed-off-by: Chiara Marcial Martínez <contact@cofuente.io>

--- server/auth/index.js
+++ server/auth/index.js
@@ -6,10 +6,10 @@ router.post('/login', async (req, res, next) => {
   try {
     const user = await User.findOne({where: {email: req.body.email}})
     if (!user) {
-      console.log('No such user found:', req.body.email)
+      console.log('No such user found:', req.body.email) // eslint-disable-line no-console
       res.status(401).send('Wrong username and/or password')
     } else if (!user.correctPassword(req.body.password)) {
-      console.log('Incorrect password for user:', req.body.email)
+      console.log('Incorrect password for user:', req.body.email) // eslint-disable-line no-console
       res.status(401).send('Wrong username and/or password')
     } else {
       req.login(user, err => (err ? next(err) : res.json(user)))
--- server/socket/index.js
+++ server/socket/index.js
@@ -1,9 +1,9 @@
 module.exports = io => {
   io.on('connection', socket => {
-    console.log(`A socket connection to the server has been made: ${socket.id}`)
+    console.log(`A socket connection to the server has been made: ${socket.id}`) // eslint-disable-line no-console
 
     socket.on('disconnect', () => {
-      console.log(`Connection ${socket.id} has left the building`)
+      console.log(`Connection ${socket.id} has left the building`) // eslint-disable-line no-console
     })
   })
 }
